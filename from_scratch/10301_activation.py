741d2e31bafd407dac754cb9b0b194dfabb8af48
loosebooster <117014473+loosebooster@users.noreply.github.com>
2023-01-25 23:19:16 +0900

    Add files via upload

---
+++ from_scratch/10301_activation.py
@@ -0,0 +1,14 @@
+# Activation Function
+# Perceptron -> Neural Network
+
+import numpy as np
+
+def step_function(x):
+    y = x > 0
+    return y.astype(int)   # old version: y.astype(np.int)
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+def relu(x):
+    return np.maximum(0,x)
