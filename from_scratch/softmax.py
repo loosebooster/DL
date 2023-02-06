e87c26fe1b1c82a59d4a9456b03674b30cc889a5
loosebooster <loosebooster@gmail.com>
2023-02-06 23:19:24 +0900

    commit

--- from_scratch/softmax.py
+++ from_scratch/softmax.py
@@ -4,7 +4,7 @@ import numpy as np
 
 def softmax(a):
     c = np.max(a)
-    exp_a = np.exp(a - c)  # Preventing overflow,  ## exponential funtion
+    exp_a = np.exp(a - c)  # Preventing overflow, Exponential funtion
     sum_exp_a = np.sum(exp_a)
     y = exp_a / sum_exp_a
     return y
