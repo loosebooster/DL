76492db51034a9a392869eb52e91c2ccfb185ca1
loosebooster <loosebooster@gmail.com>
2023-02-22 13:44:56 +0900

    commit

--- from_scratch/differentiation.py
+++ from_scratch/differentiation.py
@@ -1,2 +1,9 @@
-# Numerical differentiation
+# Numerical Differentiation
+
+## Differentiation
+def numerical_diff(f, x):
+    h = 1e-50   # 아주 작은 값
+    return (f(x + h) - f(x)) / h   # [problem] rounding error
+
+
 
