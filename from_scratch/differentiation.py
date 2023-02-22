455735ac2f96498503c37fafb47e1b655e87e1ef
loosebooster <loosebooster@gmail.com>
2023-02-22 13:59:04 +0900

    commit

--- from_scratch/differentiation.py
+++ from_scratch/differentiation.py
@@ -5,5 +5,6 @@ def numerical_diff(f, x):
     h = 1e-50   # 아주 작은 값
     return (f(x + h) - f(x)) / h   # [problem] rounding error
 
-
-
+def numerical_diff(f, x):
+    h = 1e-4   # 0.0001
+    return (f(x+h) - f(x-h)) / (2*h)   # 중심차분 or 중앙차분
